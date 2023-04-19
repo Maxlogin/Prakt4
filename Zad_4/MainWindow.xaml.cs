d180d08379543021d21c47d696d408b05935ba09
Daniel <vfgkc3@gmail.com>
2023-04-19 20:57:34 +0300

    Задание 4 сделано

---
+++ Zad_4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(txtNumber1.Text);
+            bool Da = true;
+            while (n > 1)
+            {
+                if (n % 3 != 0)
+                {
+                    Da = false;
+                    break;
+                }
+                n /= 3;
+            }
+
+            lblResult.Content = $"{ Da}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
