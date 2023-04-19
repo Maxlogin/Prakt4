0f8d79532e01efe48555a2a45fae7c8873d3dd17
Daniel <vfgkc3@gmail.com>
2023-04-19 20:45:52 +0300

    Готовое 3 задание

--- ZAD_3/MainWindow.xaml.cs
+++ ZAD_3/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace ZAD_3
         {
             InitializeComponent();
         }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(txtNumber1.Text);
+            int n = int.Parse(txtNumber2.Text);
+            double a, h, s, c;
+            switch (n)
+            {
+                case 1:
+                    a = x;
+                    c = a * Math.Sqrt(2);
+                    h = c / 2;
+                    s = c * h / 2;
+                    lblResult.Content = $"Высота равна:{h:N2}\n" +
+                                        $"Площадь равна:{s:N2}\n" +
+                                        $"Длина катета равна:{a:N2}\n" +
+                                        $"Гипатенуза равна:{c:N2}";
+                break;
+                case 2:
+                    c = x;
+                    a = c / Math.Sqrt(2);
+                    h = c / 2;
+                    s = c * h / 2;
+                    lblResult.Content = $"Высота равна:{h:N2}\n" +
+                                        $"Площадь равна:{s:N2}\n" +
+                                        $"Гипатенуза равна:{c:N2}\n" +
+                                        $"Длина катета равна:{a:N2}";
+                    break;
+                case 3:
+                    h = x;
+                    c = 2 * h;
+                    a = c / Math.Sqrt(2);
+                    s = c * h / 2;
+                    lblResult.Content = $"Гипатенуза равна:{c:N2}\n" +
+                                        $"Высота равна:{h:N2}\n" +
+                                        $"Площадь равна:{s:N2}\n" +
+                                        $"Длина катета равна:{a:N2}";
+                    break;
+                case 4:
+                    s = x;
+                    h = Math.Sqrt(s);
+                    c = 2 * h;
+                    a = c / Math.Sqrt(2);
+                    lblResult.Content = $"Гипатенуза равна:{c:N2}\n" +
+                                        $"Высота равна:{h:N2}\n" +
+                                        $"Длина катета равна:{a:N2}\n"+
+                                        $"Площадь равна:{s:N2}";
+                    break;
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
