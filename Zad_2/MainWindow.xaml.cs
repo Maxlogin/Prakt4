4c904ece0ae3c06ef1522a8a5a8b2e8cb6cfba4b
danil <danil.lashmanov2005@gmail.com>
2023-04-19 14:43:57 +0300

    Выполнил задание 2

--- Zad_2/MainWindow.xaml.cs
+++ Zad_2/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace Zad_2
         {
             InitializeComponent();
         }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int age = int.Parse(txtNumber1.Text);
+            string OpisVozrast;
+            int lastDigit = age % 10;
+            switch (lastDigit)
+            {
+                case 1:
+                    OpisVozrast = age + " год";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    OpisVozrast = age + " года";
+                    break;
+                default:
+                    OpisVozrast = age + " Лет";
+                    break;
+            }
+
+            lblResult.Content = $"{OpisVozrast}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
